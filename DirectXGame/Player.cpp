0b420146064676d101ca1c879b6a4c3b978c99ae
tojoyoshiki <146914549+tojoyoshiki@users.noreply.github.com>
2024-05-11 09:09:07 +0900

    わかりません

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,5 +1,4 @@
 #include "Player.h"
-#include <cassert>
 
 void Player::Initialize(Model* model, 
 	uint32_t textureHandle, ViewProjection* viewProjection) { 
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,4 +1,5 @@
 #pragma once
+#include<cassert>
 #include "Model.h"
 #include "WorldTransform.h"
 #include "ViewProjection.h"
